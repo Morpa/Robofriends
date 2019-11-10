b2d3db49f5ad0ce9f4473d261e4fbac473d17fcf
André Morpanini <andremorpanini@MBP-de-Andre.Home>
2019-11-10 15:42:56 +0000

    Updates

---
+++ precache-manifest.27e63159f6a468316553a224b29322f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea102666be99fd9fdcae5cfab0bcea1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8564b567b2e50ee300ca",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "87f7d361f6ebd01ad39e",
+    "url": "/robofriends/static/css/main.af824e97.chunk.css"
+  },
+  {
+    "revision": "8564b567b2e50ee300ca",
+    "url": "/robofriends/static/js/2.08bf0e13.chunk.js"
+  },
+  {
+    "revision": "87f7d361f6ebd01ad39e",
+    "url": "/robofriends/static/js/main.d2b8e505.chunk.js"
+  },
+  {
+    "revision": "cb519fe66fcbb61e7a5c",
+    "url": "/robofriends/static/js/runtime-main.0560b89d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
